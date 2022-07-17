084d1b5916512b66d6343509806b4355e96b771e
Ben Smus <bensmus@gmail.com>
2022-07-17 11:43:46 -0700

    python mysql connector

---
+++ create_tables.py
@@ -0,0 +1,20 @@
+import mysql.connector
+
+db = mysql.connector.connect(
+    host="database-1.crb52cpzkqln.us-west-1.rds.amazonaws.com",
+    passwd="speedyfoods321",
+    user="admin",
+    database="testdatabase"
+)
+
+mycursor = db.cursor()
+
+# create_person_table_query = "CREATE TABLE Person (name VARCHAR(50), age smallint UNSIGNED, personID int PRIMARY KEY AUTO_INCREMENT)"
+# mycursor.execute(create_person_table_query)
+
+mycursor.execute("DESCRIBE Person")
+
+for x in mycursor:
+    print(x)
+
+db.close()
