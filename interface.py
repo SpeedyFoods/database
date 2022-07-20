0cf97cd995c0146e307228447c07e6d557565f7e
tomi <tomilui1221@gmail.com>
2022-07-20 00:50:41 -0700

    some interface functionalities

---
+++ interface.py
@@ -0,0 +1,18 @@
+# what are the functionalities that our app should have?
+# create decision tree
+
+
+"""
+1. sign up
+ - credit card
+ - address
+ - sing up as speeder
+
+2. register a restaurant
+
+3. place an order
+- order item?
+
+4. how do we keep track of delivery?
+
+ """
